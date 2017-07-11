6489d59cd84b11d8323926f603d0ad6912723e7c
Brendan Bard <bbard@purdue.edu>
2017-07-11 11:20:35 -0400

    added message for github submit route

--- src/Github.js
+++ src/Github.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Route} from 'react-router-dom'
 import './Github.css'
 
 class Github extends Component {
@@ -12,12 +13,14 @@ class Github extends Component {
     }
     handleSubmit = (ev) =>{
         ev.preventDefault()
-        this.props.history.push(`github/${this.state.username}`)
+        this.props.history.push(`/github/${this.state.username}`)
     }   
     render() {
         return (
             <div className="github">
-                <img className='github-logo' src='http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png' alt='github logo' />
+                <img className='github-logo' 
+                src='http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png' 
+                alt='github logo' />
                 <form onSubmit={this.handleSubmit}>
                     <div>
                         <input type='text' value={this.state.username} onChange={this.handleChange} />
@@ -26,6 +29,7 @@ class Github extends Component {
                         <button>Look up Github user</button>
                     </div>
                 </form>
+                <Route exact path ='/github' render={() => <h3>Please enter a username to search on Github </h3>} />
             </div>
         )
     }
